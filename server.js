ad2d58c7c8d6ad8aa26ba1dfb0452b3eb3948798
JakubW <wiechniak.j@gmail.com>
2020-12-20 11:03:41 +0100

    sixth commit

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ const path = require('path');
 const app = express();   
 
 // Serve only the static files form the dist directory    
-app.use(express.static(__dirname + '/dist/off-angular-deployment-jw-tg'));
+app.use(express.static(__dirname + '/dist/an-angular-python-app'));
 
 app.get('/*', function(req,res) {  
-    res.sendFile(path.join(__dirname+'/dist/off-angular-deployment-jw-tg/index.html'));   
+    res.sendFile(path.join(__dirname+'/dist/an-angular-python-app/index.html'));   
 });  
 
 // Start the app by listening on the default Heroku port    
